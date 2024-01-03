0a6fae602220cdd7a16f1980b337a8c372c26c1f
Adam Marianowski <amarianowski@outlook.com>
2024-01-03 16:06:12 +0100

    commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,7 +3,7 @@ import { navRoutes as routes } from "../utils/navRoutes";
 
 export default function Nav() {
   return (
-    <nav className="">
+    <nav>
       <ul className="nav nav-pills">
         {routes.map((route) => (
           <NavLink key={route.path} to={route.path} className="nav-link">
